fb15f9d586481d4629cd5ec58090f9996116abe3
Amit Chakraborty <amitchakraborty1097@gmail.com>
2021-06-08 16:26:01 +0530

    resolved merge

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -3,10 +3,7 @@ package com.example.demo.model;
 // Employee class
 public class Employee {
 
-<<<<<<< HEAD
-	//employee Id
-=======
->>>>>>> 5f860572c2eaec4a9e4866697b67b8caa821642b
+	//employee Id 
 	private String employeeId;
 	private String employeeName;
 }
